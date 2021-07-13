64f5cbcc52a55e5a532482c1a4ce412da44fccd6
Rahmad3067 <aboozar919@gmail.com>
2021-07-13 09:23:01 +0200

    finis

--- chapitre_01/elsy/src/App.js
+++ chapitre_01/elsy/src/App.js
@@ -1,16 +1,17 @@
 import React from "react";
-import Box from './components/Box'
-import 'bootstrap/dist/css/bootstrap.min.css'
+import Box from './components/Box';
+import './styles/bootstrap.css';
+import './styles/global.css';
 import './App.css';
-import './styles/global.css'
 
 
-const tempMin = "-20"
-const tempMax = 40
-const heartMin = 80
-const heartMax = 180
-const stepsMin = 0
-const stepsMax = 50000
+
+const tempMin = -20;
+const tempMax = 40;
+const heartMin = 80;
+const heartMax = 180;
+const stepsMin = 0;
+const stepsMax = 50000;
 
 class App extends React.Component {
   render() {
@@ -18,10 +19,6 @@ class App extends React.Component {
       <div className = "container-fluid">
         <h1 className="text-primary">Hello !</h1>
         <div className= "row">
-        <p>Heart : {heartMin} </p>
-          <p>temperature : {tempMin} </p>
-          <p>Box : {stepsMin} </p>
-          <Box />
           <Box icon='local_drink' color='#3A85FF' value='1.5' unit='L' />
           <Box icon="directions_walk" color="black" value="3000" unit='steps' />
           <Box icon="favorite" color="red" value="120" unit='bpm' />
